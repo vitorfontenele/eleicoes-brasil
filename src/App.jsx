afe340d3e831fd88161003d3d8dc96a1d4e45012
vitor-fontenele <vitor_fontenele@hotmail.com>
2022-12-07 21:24:59 -0300

    Adjust states

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,16 @@ import states from "./states/states.json";
 function App() {
   const [now, setNow] = useState("");
   const [statesResults, setStatesResults] = useState(states);
-  const [countryResults, setCountryResults] = useState({"apurado-brasil": 0, "candidatos": []});
+  const [computedCountry, setComputedCountry] = useState("");
+  const [candidates, setCandidates] = useState([]);
 
   useEffect(() => {
     setNow(new Date(Date.now()));
-    getResultsCountry();
+    getData();
   }, [])
 
-  const getResultsCountry = async () => {
-    const countryResultsCopy = {...countryResults};
+  const getData = async () => {
+    const candidatesCopy = [...candidates];
     const countryURL = baseURL("br");
     let response;
     let data;
@@ -24,18 +25,16 @@ function App() {
     } catch(error) {
       console.log(error)
     }
-    countryResultsCopy["apurado-brasil"] = data["pst"].replace(",", ".");
+    setComputedCountry(data["pst"].replace(",", "."));
     data["cand"].map((candidato, index) => {
-      countryResultsCopy["candidatos"][index] = candidato;
-      //countryResultsCopy[`total${candidato["n"]}`] = 0;
+      candidatesCopy[index] = candidato;
     })
-    countryResultsCopy[`totalGeral`] = 0;
-    setCountryResults(countryResultsCopy);
+    setCandidates(candidatesCopy);
   }
 
   useEffect(() => {
     getResultsStates();
-  }, [countryResults])
+  }, [candidates])
 
   const getResultsStates = () => {
     const statesResultsCopy = [...statesResults];
@@ -58,21 +57,17 @@ function App() {
           state[`${uf}-perc-${n}`] = perc;
           const totalAbs = Math.round(perc * 0.01 * state["eleitores"]);
           state[`${uf}-total-${n}`] = totalAbs;             
-          //countryResultsCopy[`total${n}`] += totalAbs;
-          //countryResultsCopy["totalGeral"] += totalAbs;
         })
     })
-    //console.log(countryResultsCopy);
     setStatesResults(statesResultsCopy);
-    // setCountryResults(countryResultsCopy);
   }
 
   const totals = {};
-  countryResults["candidatos"].map(result => {
+  candidates.map(result => {
     totals[`total${result["n"]}`] = 0;
   })
 
-  countryResults["candidatos"].map(result => {
+  candidates.map(result => {
     statesResults.map(state => {
       totals[`total${result["n"]}`] += state[`${state["abbr"]}-total-${result["n"]}`]
     })
@@ -87,12 +82,12 @@ function App() {
   return (
     <div className="App">
       <h1>Previsão do Resultado Final</h1>
-      <h2 id="apurado-brasil">{`Apurado Brasil: ${countryResults["apurado-brasil"]} %`}</h2>
+      <h2 id="apurado-brasil">{`${computedCountry} %`}</h2>
       <h4 id="hora">{`Hora: ${now}`}</h4>
       <table className="table mb-5">
         <thead className="thead-dark">
           <tr style={{fontSize: "28px"}}>
-            {countryResults["candidatos"].map(result => {
+            {candidates.map(result => {
               return (
                 <>
                   <th>{result["nm"]}</th>
@@ -103,7 +98,7 @@ function App() {
         </thead>
         <tbody>
           <tr style={{fontSize: "40px"}}>
-            {countryResults["candidatos"].map(result => {
+            {candidates.map(result => {
               return (
                 <>
                   <td>{totals[`total${result["n"]}`]}</td>
@@ -121,7 +116,7 @@ function App() {
             <th>NOME UF</th>
             <th>APURADO</th>
             <th>ELEITORES</th>
-            {countryResults["candidatos"].map(result => {
+            {candidates.map(result => {
               return (
                 <>
                   <th>{`${result["nm"]} (%)`}</th>
@@ -139,7 +134,7 @@ function App() {
                 <th>{state.nome}</th>
                 <th>{`${state[`${state["abbr"]}-apurado`]} %`}</th>
                 <th>{state.eleitores}</th>
-                {countryResults["candidatos"].map(result => {
+                {candidates.map(result => {
                   return (
                     <>
                       <th>{state[`${state["abbr"]}-perc-${result["n"]}`]}</th>
