047f10fd2cc14dd661a924fe1000e70ff104af14
vitor-fontenele <vitor_fontenele@hotmail.com>
2022-12-07 21:43:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -63,13 +63,13 @@ function App() {
   }
 
   const totals = {};
-  candidates.map(result => {
-    totals[`total${result["n"]}`] = 0;
+  candidates.map(candidate => {
+    totals[`total${candidate["n"]}`] = 0;
   })
 
-  candidates.map(result => {
+  candidates.map(candidate => {
     statesResults.map(state => {
-      totals[`total${result["n"]}`] += state[`${state["abbr"]}-total-${result["n"]}`]
+      totals[`total${candidate["n"]}`] += state[`${state["abbr"]}-total-${candidate["n"]}`]
     })
   })
 
@@ -82,15 +82,15 @@ function App() {
   return (
     <div className="App">
       <h1>Previsão do Resultado Final</h1>
-      <h2 id="apurado-brasil">{`${computedCountry} %`}</h2>
+      <h2 id="apurado-brasil">{`Apurado Brasil: ${computedCountry}%`}</h2>
       <h4 id="hora">{`Hora: ${now}`}</h4>
       <table className="table mb-5">
         <thead className="thead-dark">
           <tr style={{fontSize: "28px"}}>
-            {candidates.map(result => {
+            {candidates.map(candidate => {
               return (
                 <>
-                  <th>{result["nm"]}</th>
+                  <th>{candidate["nm"]}</th>
                 </>
               )
             })}
@@ -98,10 +98,10 @@ function App() {
         </thead>
         <tbody>
           <tr style={{fontSize: "40px"}}>
-            {candidates.map(result => {
+            {candidates.map(candidate => {
               return (
                 <>
-                  <td>{totals[`total${result["n"]}`]}</td>
+                  <td>{`${totals[`total${candidate["n"]}`]}%`}</td>
                 </>
               )
             })}
@@ -116,11 +116,11 @@ function App() {
             <th>NOME UF</th>
             <th>APURADO</th>
             <th>ELEITORES</th>
-            {candidates.map(result => {
+            {candidates.map(candidate => {
               return (
                 <>
-                  <th>{`${result["nm"]} (%)`}</th>
-                  <th>{`${result["nm"]} (ABS)`}</th>
+                  <th>{`${candidate["nm"]} (%)`}</th>
+                  <th>{`${candidate["nm"]} (ABS)`}</th>
                 </>
               )
             })}
@@ -130,15 +130,15 @@ function App() {
           {statesResults.map((state, index) => {
             return (
               <tr key={index}>
-                <th>{state.abbr}</th>
-                <th>{state.nome}</th>
-                <th>{`${state[`${state["abbr"]}-apurado`]} %`}</th>
-                <th>{state.eleitores}</th>
-                {candidates.map(result => {
+                <td>{state.abbr}</td>
+                <td>{state.nome}</td>
+                <td>{`${state[`${state["abbr"]}-apurado`]}%`}</td>
+                <td>{state.eleitores}</td>
+                {candidates.map(candidate => {
                   return (
                     <>
-                      <th>{state[`${state["abbr"]}-perc-${result["n"]}`]}</th>
-                      <th>{state[`${state["abbr"]}-total-${result["n"]}`]}</th>
+                      <td>{`${state[`${state["abbr"]}-perc-${candidate["n"]}`]}%`}</td>
+                      <td>{state[`${state["abbr"]}-total-${candidate["n"]}`]}</td>
                     </>
                   )
                 })}
